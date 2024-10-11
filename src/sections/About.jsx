857c98477af0390e426cc7050e3c6c19476f204f
MiguelOliveira63 <migueloliveira6333@gmail.com>
2024-10-11 18:52:14 +0100

    About finalizado

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Globe from "react-globe.gl";
 import Button from "../components/Button";
 
 const About = () => {
+
+  const [hasCopied, setHasCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText("migueloliveira6333@gmail.com");
+    setHasCopied(true);
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  }
   return (
     <section className="c-scpace my-20">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-rows-3 grid-cols-1 gap-5 h-full">
@@ -46,7 +55,27 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className="">
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
+
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">I love solving prblems and building things through code. Coding isn't just my profession - it is my passion.</p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img src="assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[276px] h-fit object-contain sm:object-top"/>
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg' } alt="copy" />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">migueloliveira6333@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
